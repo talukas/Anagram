539b0e580603cb4a8fc74e5a07610af86120037d
Tania <taluk@outlook.com>
2017-05-14 15:12:59 -0700

    False Anogram

--- src/core/App.java
+++ src/core/App.java
@@ -3,7 +3,7 @@ package core;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+		System.out.println("Охренеть and Не хрена себе is anagram? - " + Anagram.validate("Охренеть", "Не хрена себе"));
 
 	}
 
